4b0988377048f9cef2007764dba9d02480ac6181
JensIssa <71692396+JensIssa@users.noreply.github.com>
2023-04-13 06:31:09 +0200

    video assigment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {FireService} from "./fire.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'videoassignment';
+  sendThisMessage: any;
+
+  constructor(public fireService: FireService) {
+  }
 }
---
+++ src/app/fire.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/app'
+
+import * as config from '../../firebaseconfig.js'
+@Injectable({
+  providedIn: 'root'
+})
+export class FireService {
+
+  firebaseApplication;
+  firestore: firebase.firestore.Firestore;
+  messages: any[] = [];
+
+  constructor() {
+    this.firebaseApplication = firebase.initializeApp(config.firebaseConfig);
+    this.firestore = firebase.firestore();
+    this.getMessages();
+  }
+
+  sendMessage(sendThisMessage: any) {
+    let messageDTO: MessageDTO = {
+      messageContent: sendThisMessage,
+      timestamp: new Date(),
+      user: 'Some user'
+    }
+    this.firestore
+      .collection('myChat')
+      .add(messageDTO)
+
+  }
+
+  getMessages() {
+    this.firestore
+      .collection('myChat')
+      .where('user', '==', 'some user')
+      .onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type == "added") {
+            this.messages.push({id: change.doc.id, data: change.doc.data()});
+          }
+          if (change.type == 'modified') {
+            const index = this.messages.findIndex(document => document.id != change.doc.id);
+            this.messages[index] =
+              {id: change.doc.id, data: change.doc.data()}
+          }
+          if (change.type == "removed") {
+            this.messages = this.messages.filter(m => m.id != change.doc.id);
+          }
+        })
+      })
+  }
+}
+export interface MessageDTO {
+  messageContent: string;
+  timestamp: Date;
+  user: string;
+}
